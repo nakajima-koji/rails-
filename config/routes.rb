22d760a45a2ad4baf0c2dbe6946cad1ed0389bea
nakajima-koji <crazy.max.meteora@gmail.com>
2020-03-24 03:38:52 +0000

    create終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  get 'about', to: 'home#about'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/index'
+  get 'posts/:id', to: 'posts#show', as: :post
 end
